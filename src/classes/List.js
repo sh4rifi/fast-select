145f8b2284519d3d18fdcece89b5b371274977c6
Ali Sharifineyestani <sh4rifi@gmail.com>
2020-03-29 18:48:42 +0430

    add List class

---
+++ src/classes/List.js
@@ -0,0 +1,34 @@
+export class List {
+
+    constructor(array = []) {
+        this.array = array;
+    };
+
+    range = (start, stop, step = 1) => {
+        this.array = [start];
+        while (start < stop) {
+            this.array.push(start += step);
+        }
+        return this;
+    };
+
+    breakUp = () => {
+        let tmp, current, top = this.array.length;
+        if (top) while (--top) {
+            current = Math.floor(Math.random() * (top + 1));
+            tmp = this.array[current];
+            this.array[current] = this.array[top];
+            this.array[top] = tmp;
+        }
+        return this;
+    };
+
+    replaceItem = (oldItem, newItem) => {
+        this.array = this.array.map(item => item === oldItem ? newItem : item);
+        return this;
+    }
+
+    get = () => (this.array)
+
+}
+
