7b0227232e54ecbd8201d6f8078a7aaf7efbea6b
Ali Sharifineyestani <sh4rifi@gmail.com>
2020-03-30 00:43:32 +0430

    add timer and add random collection

--- src/scenes/Game1to50/index.js
+++ src/scenes/Game1to50/index.js
@@ -2,17 +2,44 @@ import React, {useState} from 'react';
 import SectionHeader from "../../components/SectionHeader";
 import Cell from "./components/Cell"
 import Button from "../../components/Button";
-
+import {List} from "../../classes/List";
 import './style.scss';
 
 
+
+
 function Game1to50() {
-    const [collection, setCollection] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]);
+
+    const start = 1;
+    const end = 5;
+    const level = 1;
+    const step = 1;
+    const arr = new List();
+
+
+
+    const [firstCollection] = useState(arr.range(start, (end / level), step).breakUp().get());
 
     /*_______________________________________timer_________________________________________________________________*/
 
     const [elapsed, setElapsed] = useState(0);
+    const [increment, setIncrement] = useState(0);
+    const startTimer = () => {
+        setIncrement(
+            setInterval(() => {
+                setElapsed(elapsed => elapsed + 0.01);
+            }, 10)
+        )
 
+    };
+    const resetTimer = () => {
+        stopTimer();
+        setElapsed(0);
+    };
+    const stopTimer = () => {
+        clearInterval(increment);
+
+    };
     const getMillisecond = () => {
         return ('0' + elapsed * 100).slice(-2);
     };
@@ -21,6 +48,15 @@ function Game1to50() {
     };
     const getMinute = () => {
         return ('0' + Math.floor(elapsed / 60)).slice(-2);
+    };
+
+
+
+    const handleClick = (number) => {
+        if (number === start)
+            startTimer();
+        else if (number === end)
+            stopTimer();
     };
 
     return (
@@ -39,10 +75,12 @@ function Game1to50() {
                 <div className="container">
                     <div className="container-table max-w-576 div-center">
                         <div className="table">
-                            {collection.map((number, i) => (
+                            {firstCollection.map((number, i) => (
                                 <Cell
                                     key={i}
                                     value={number}
+                                    handleClick={() => handleClick(number)}
+                                    className={["box", number > (end / 2) ? "selected" : "flash"].join(" ")}
                                 />
                             ))}
                         </div>
@@ -55,6 +93,7 @@ function Game1to50() {
                         <Button
                             title="Reset"
                             size="button-lg"
+                            handleClick={() => resetTimer()}
                             type="button-block button-primary"
                         />
                     </div>
